f573896312e8f016229861dd6a5d2791ddc9279d
Ryan Ross <ryan@reross.com>
2022-04-01 10:43:34 -0500

    solved react hook drills #2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import PokemonList from "./components/PokemonList";
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=9")
+      .then((res) => setPokemon(res.data.results));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      {pokemon.map((pokemon, index) => {
+        return <PokemonList pokemon={pokemon} key={index} />;
+      })}
     </div>
   );
 }
---
+++ app-2/src/components/PokemonList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PokemonList = ({ pokemon }) => {
+  return <h3>{pokemon.name}</h3>;
+};
+
+export default PokemonList;
